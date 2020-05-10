a4f19c1637e02f600bce464039f9c5936fcccea5
Can Deng <candeng@umich.edu>
2020-05-10 10:16:24 -0700

    completed proj2c

--- proj2c-19/bearmaps/proj2c/MapServerInitializer.java
+++ proj2c-19/bearmaps/proj2c/MapServerInitializer.java
@@ -20,7 +20,7 @@ public class MapServerInitializer {
      * Do not place it in the main function. Do not place initialization code anywhere else.
      **/
     public static void initializeServer(Map<String, APIRouteHandler> apiHandlers){
-
+        port(getHerokuAssignedPort());
         Constants.SEMANTIC_STREET_GRAPH = new AugmentedStreetMapGraph(Constants.OSM_DB_PATH);
         staticFileLocation("/page");
         /* Allow for all origin requests (since this is not an authenticated server, we do not
@@ -41,5 +41,13 @@ public class MapServerInitializer {
         }
 
 
+    }
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
