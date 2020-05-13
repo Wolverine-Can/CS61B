deb66d7a0f6ae39889d78834a10072e352719f0b
Can Deng <candeng@umich.edu>
2020-05-13 10:08:45 -0700

    modified hw5

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -5,10 +5,10 @@ import java.util.LinkedList;
 public class SeamCarver {
     private Picture picture;
     public SeamCarver(Picture picture) {
-        this.picture = new Picture(picture) ;
+        this.picture = new Picture(picture);
     }
     public Picture picture() {
-        return picture;
+        return new Picture(picture);
     }
     public     int width() {
         return picture.width();
@@ -39,29 +39,29 @@ public class SeamCarver {
         return xEnergy + yEnergy;
     }
     private double xEnergyHelper(int x1, int x2, int y) {
-        return  Math.pow(picture.get(x1,y).getRed() - picture.get(x2,y).getRed(), 2)
-                + Math.pow(picture.get(x1,y).getGreen() - picture.get(x2,y).getGreen(), 2)
-                + Math.pow(picture.get(x1,y).getBlue() - picture.get(x2,y).getBlue(), 2);
+        return  Math.pow(picture.get(x1, y).getRed() - picture.get(x2, y).getRed(), 2)
+                + Math.pow(picture.get(x1, y).getGreen() - picture.get(x2, y).getGreen(), 2)
+                + Math.pow(picture.get(x1, y).getBlue() - picture.get(x2, y).getBlue(), 2);
     }
     private double yEnergyHelper(int y1, int y2, int x) {
-        return  Math.pow(picture.get(x,y1).getRed() - picture.get(x,y2).getRed(), 2)
-                + Math.pow(picture.get(x,y1).getGreen() - picture.get(x,y2).getGreen(), 2)
-                + Math.pow(picture.get(x,y1).getBlue() - picture.get(x,y2).getBlue(), 2);
+        return  Math.pow(picture.get(x, y1).getRed() - picture.get(x, y2).getRed(), 2)
+                + Math.pow(picture.get(x, y1).getGreen() - picture.get(x, y2).getGreen(), 2)
+                + Math.pow(picture.get(x, y1).getBlue() - picture.get(x, y2).getBlue(), 2);
     }
-    public int[] findHorizontalSeam(){
-        Picture TransposePicture = new Picture(this.picture.height(), this.picture.width());
+    public int[] findHorizontalSeam() {
+        Picture transposePicture = new Picture(this.picture.height(), this.picture.width());
         for (int x = 0; x < this.picture.width(); x++) {
             for (int y = 0; y < this.picture.height(); y++) {
-                TransposePicture.set(y, x, this.picture.get(x, y));
+                transposePicture.set(y, x, this.picture.get(x, y));
             }
         }
-        SeamCarver TransposeSearCarver = new SeamCarver(TransposePicture);
-        return TransposeSearCarver.findVerticalSeam();
+        SeamCarver transposeSearCarver = new SeamCarver(transposePicture);
+        return transposeSearCarver.findVerticalSeam();
     }
 
-    public int[] findVerticalSeam(){
-        LinkedList<Integer>[] preAllSeam= new LinkedList[picture.width()];
-        LinkedList<Integer>[] AllSeam= new LinkedList[picture.width()];
+    public int[] findVerticalSeam() {
+        LinkedList<Integer>[] preAllSeam = new LinkedList[picture.width()];
+        LinkedList<Integer>[] allSeam = new LinkedList[picture.width()];
         for (int i = 0; i < picture.width(); i++) {
             preAllSeam[i] = new LinkedList<>();
         }
@@ -69,7 +69,7 @@ public class SeamCarver {
         double[] totalEnergy = new double[picture.width()];
         int minPreIndex;
         int minTotalIndex = 0;
-        int[] Seam = new int[picture.height()];
+        int[] seam = new int[picture.height()];
         for (int x = 0; x < picture.width(); x++) {
             preEnergy[x] = energy(x, 0);
             totalEnergy[x] = energy(x, 0);
@@ -77,31 +77,32 @@ public class SeamCarver {
         }
         for (int y = 1; y < picture.height(); y++) {
             for (int x = 0; x < picture.width(); x++) {
-                AllSeam[x] = new LinkedList<>();
+                allSeam[x] = new LinkedList<>();
                 minPreIndex = minPreEnergyIndex(x, preEnergy);
                 totalEnergy[x] = energy(x, y) + preEnergy[minPreIndex];
-                AllSeam[x].addAll(preAllSeam[minPreIndex]);
-                AllSeam[x].add(x);
+                allSeam[x].addAll(preAllSeam[minPreIndex]);
+                allSeam[x].add(x);
             }
             for (int x = 0; x < picture.width(); x++) {
                 preAllSeam[x] = new LinkedList<>();
-                preAllSeam[x].addAll(AllSeam[x]);
+                preAllSeam[x].addAll(allSeam[x]);
             }
             System.arraycopy(totalEnergy, 0, preEnergy, 0, picture.width());
         }
         for (int x = 0; x < picture.width(); x++) {
             minTotalIndex = totalEnergy[minTotalIndex] < totalEnergy[x] ? minTotalIndex : x;
         }
-        for (int i = 0; i < Seam.length; i++) {
-            Seam[i] = AllSeam[minTotalIndex].removeFirst();
+        for (int i = 0; i < seam.length; i++) {
+            seam[i] = allSeam[minTotalIndex].removeFirst();
         }
-        return Seam;
+        return seam;
     }
     private int minPreEnergyIndex(int x, double[] preEnergy) {
         if (x == 0) {
             return preEnergy[0] < preEnergy[1] ? 0 : 1;
         } else if (x == picture.width() - 1) {
-            return preEnergy[picture.width() - 1] < preEnergy[picture.width() - 2] ? picture.width() - 1 : picture.width() - 2;
+            return preEnergy[picture.width() - 1] < preEnergy[picture.width() - 2]
+                    ? picture.width() - 1 : picture.width() - 2;
         } else {
             return threeMinIndex(x - 1, x, x + 1, preEnergy);
         }
@@ -116,7 +117,7 @@ public class SeamCarver {
     public void removeHorizontalSeam(int[] seam) {
         SeamRemover.removeHorizontalSeam(picture, seam);
     }
-    public void removeVerticalSeam(int[] seam){
+    public void removeVerticalSeam(int[] seam) {
         SeamRemover.removeVerticalSeam(picture, seam);
     }
 
